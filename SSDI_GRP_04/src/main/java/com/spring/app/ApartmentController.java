3552c3769e825b680e2b4002678d72da9eac132e
HARSH <HARSH@FORCE>
2017-03-29 21:54:50 -0400

    test2

--- SSDI_GRP_04/src/main/java/com/spring/app/ApartmentController.java
+++ SSDI_GRP_04/src/main/java/com/spring/app/ApartmentController.java
@@ -17,7 +17,7 @@ public class ApartmentController {
 	
 	private ApartmentService available_apartmentService;
 
-	/*This is just a test*/
+	/*This is just a test2*/
 	
 	@Autowired(required=true)
 	@Qualifier(value = "apartmentService")
