3d6bfca4540020b934c6be069341b8db0ff5270e
HARSH <HARSH@FORCE>
2017-04-26 22:38:17 -0400

    DAO and Service tests upgraded along with Servlet resource file for testdb

--- SSDI_GRP_04/src/test/java/com/spring/test/DAO/ApartmentDAOTest.java
+++ SSDI_GRP_04/src/test/java/com/spring/test/DAO/ApartmentDAOTest.java
@@ -1,51 +1,83 @@
 package com.spring.test.DAO;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.dialect.function.AvgWithArgumentCastFunction;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.test.context.web.WebAppConfiguration;
 import com.spring.app.dao.ApartmentDAOImpl;
 import com.spring.app.model.Available_apartment;
 
-@ContextConfiguration(locations = "classpath:servlet-context.xml")
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:servlet-apt-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
+@EnableAutoConfiguration
 public class ApartmentDAOTest {
 	
 	@Autowired
 	private ApartmentDAOImpl apartmentDAO;
 	
-	private SessionFactory sessionFactory;
-	
-	public void setSessionFactory(SessionFactory sf){
-		this.sessionFactory = sf;
-	}
+	@Autowired
+	private SessionFactory sessionFactory1;
 	
 	@Test
 	@Rollback(true)
 	public void saveApartmentDAO() {
-		Available_apartment a = new Available_apartment("9306", 111, 1, 1234);
+		Available_apartment a = new Available_apartment("990", 1500, 3, 1400);
 		
 		apartmentDAO.addAvailableApartment(a);
 		
-		 Session session = sessionFactory.openSession();
-		 Available_apartment a1 = (Available_apartment) session.get(Available_apartment.class,"9306");
-         assertEquals("pass",9306,a1.getUnit());
+		 Session session = sessionFactory1.openSession();
+		 Available_apartment a1 = (Available_apartment) session.get(Available_apartment.class,"990");
+         assertEquals("pass","990",a1.getUnit());
+        assertEquals("pass",1500,a1.getRent());
+         assertEquals("pass",3,a1.getBhk());
+         assertEquals("pass",1400,a1.getArea());
+         session.close();
+	}
+
+	@Test
+	@Rollback(true)
+	public void ListApartmentDAO() {
+		Available_apartment a = new Available_apartment("150", 1500, 3, 1400);
+
+		Session session=this.sessionFactory1.openSession();
+		session.beginTransaction();
+		session.save(a);
+		session.getTransaction().commit();
+		session.close();
+		
+		List<Available_apartment> list = apartmentDAO.listApartments();
+		 assertEquals("pass","150",list.get(list.size()-1).getUnit());
+		 assertEquals("pass",1500,list.get(list.size()-1).getRent());
+		 assertEquals("pass",3,list.get(list.size()-1).getBhk());
+		 assertEquals("pass",1400,list.get(list.size()-1).getArea());
+	}
+	
+	@Test
+	@Rollback(true)
+	public void DeleteApartmentDAO() {
+		Available_apartment a = new Available_apartment("1110", 1500, 3, 1400);
+
+		Session session=this.sessionFactory1.openSession();
+		session.beginTransaction();
+		session.save(a);
+		session.getTransaction().commit();
+		session.close();
+//		
+		List<Available_apartment> list = apartmentDAO.listApartments();
+		 assertEquals("pass","1110",list.get(list.size()-1).getUnit());
+		 assertEquals("pass",1500,list.get(list.size()-1).getRent());
+		 assertEquals("pass",3,list.get(list.size()-1).getBhk());
+		 assertEquals("pass",1400,list.get(list.size()-1).getArea());
 	}
 
 }
--- SSDI_GRP_04/src/test/java/com/spring/test/DAO/OccupiedApartmentDAOTest.java
+++ SSDI_GRP_04/src/test/java/com/spring/test/DAO/OccupiedApartmentDAOTest.java
@@ -1,126 +1,78 @@
 package com.spring.test.DAO;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
 
 import java.sql.Date;
-import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.spring.app.dao.Occupied_apartmentDAOImpl;
+import com.spring.app.model.Available_apartment;
 import com.spring.app.model.Occupied_apartment;
-import com.spring.app.model.User;
-import com.spring.app.service.UserService;
-import com.spring.app.service.UserServiceImpl;
 
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:servlet-apt-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@EnableAutoConfiguration
 public class OccupiedApartmentDAOTest {
-
-	private static Occupied_apartmentDAOImpl occaptDaoImplMock;
-	private static Occupied_apartment occapt1;
-	private static Occupied_apartment occapt2;
 	
-	private static Occupied_apartment occaptmock;
-
-	private static UserServiceImpl userservicemockimpl;
-	private static com.spring.app.model.User User, User2;
-
-
+	@Autowired
+	private SessionFactory sessionFactory1;
 	
-	@BeforeClass
-	public static void setup(){
-		userservicemockimpl=mock(UserServiceImpl.class);
-		User=mock(User.class);
-		User2=mock(User.class);
-		User=new User(); User2=new User();
-		occaptDaoImplMock=mock(Occupied_apartmentDAOImpl.class);
-		Date d=new Date(0);
-		occapt1=new Occupied_apartment();
-		occapt2=new Occupied_apartment();
-		occapt1.setBill(45);
-		occapt1.setEmail("sneha@g");
-		occapt1.setLeaseEnd(d);
-		occapt1.setLeaseStart(d);
-		occapt1.setUnit("9505J");
-
-		occapt2.setBill(35);
-		occapt2.setEmail("snehaV@g");
-		occapt2.setLeaseEnd(d);
-		occapt2.setLeaseStart(d);
-		occapt2.setUnit("9450");
-
-		User.setEmail("snehaV@g");
-		User.setUnit("9450");
+	@Autowired 
+	private Occupied_apartmentDAOImpl occuAptDAO;
+	@Test
+	@Rollback(true)
+	public void saveApartmentDAO() {
+		Occupied_apartment occuaptmnt= new Occupied_apartment();
+		Calendar currenttime = Calendar.getInstance();
+		Date sqlCurrentdate = new Date((currenttime.getTime()).getTime());
+		currenttime.add(Calendar.YEAR, 1);
+		Date sqlYeardate = new Date((currenttime.getTime()).getTime());
+		occuaptmnt.setUnit("999");
+		occuaptmnt.setEmail("aaa");
+		occuaptmnt.setLeaseStart(sqlCurrentdate);
+		occuaptmnt.setLeaseEnd(sqlYeardate);
+		occuaptmnt.setBill(0);
 		
-		User2.setEmail("sneha@g");
-		User2.setUnit("9505J");
+		occuAptDAO.addOccupiedApartment(occuaptmnt);
 		
-		when(occaptDaoImplMock.occupiedApartmentsList()).thenReturn(Arrays.asList(occapt1,occapt2));
-		when(userservicemockimpl.listUsers()).thenReturn(Arrays.asList(User, User2));
-
+		 Session session = sessionFactory1.openSession();
+		 Occupied_apartment a1 = (Occupied_apartment) session.get(Occupied_apartment.class,"999");
+         assertEquals("pass","999",a1.getUnit());
+         assertEquals("pass","aaa",a1.getEmail());
+         session.close();
 	}
 
 	@Test
-	public void testGetAllOccApt() {
-		List<Occupied_apartment> occlist=occaptDaoImplMock.occupiedApartmentsList();
-		assertEquals(2, occlist.size());
-		Occupied_apartment o=occlist.get(0);
-		assertEquals("9505J",o.getUnit());
-	}
-
-	@Test
-	public void testAddOccApt() {
-		Occupied_apartment occuaptmnt = mock(Occupied_apartment.class);
-		occuaptmnt.setEmail("ex@ex.ex");
-		occuaptmnt.setBill(1);
-		occuaptmnt.setUnit("Example unit");
-		occaptDaoImplMock.addOccupiedApartment(occuaptmnt);
-		List<Occupied_apartment> occlist = occaptDaoImplMock.occupiedApartmentsList();
-		for(Occupied_apartment o : occlist){
-			if("Example unit".equals(o.getUnit())){
-				assertEquals("Example unit", o.getUnit());
-				assertEquals("ex@ex.ex", o.getEmail());
-				
-			}
-		}
-	}
-
-	@Test
-	public void testDeleteOccApt() {
-		String unit="9523";
-		String email=null;
+	@Rollback(true)
+	public void ListApartmentDAO() {
+		Occupied_apartment occuaptmnt= new Occupied_apartment();
+		Calendar currenttime = Calendar.getInstance();
+		Date sqlCurrentdate = new Date((currenttime.getTime()).getTime());
+		currenttime.add(Calendar.YEAR, 1);
+		Date sqlYeardate = new Date((currenttime.getTime()).getTime());
+		occuaptmnt.setUnit("999");
+		occuaptmnt.setEmail("a11");
+		occuaptmnt.setLeaseStart(sqlCurrentdate);
+		occuaptmnt.setLeaseEnd(sqlYeardate);
+		occuaptmnt.setBill(0);
 		
-		Occupied_apartment occuaptmnt = new Occupied_apartment();
-		occuaptmnt.setEmail("ex@ex.ex");
-		occuaptmnt.setBill(1);
-		occuaptmnt.setUnit("Example unit");
-		occuaptmnt.setUnit("9523");
+		occuAptDAO.addOccupiedApartment(occuaptmnt);
 		
-		occaptDaoImplMock.addOccupiedApartment(occuaptmnt);
-		
-		List<Occupied_apartment> occlist = occaptDaoImplMock.occupiedApartmentsList();
-		occaptmock=mock(Occupied_apartment.class);
-	
-		for (Occupied_apartment occaptmock: occlist) {
-		if(occaptmock.getUnit().equals(unit)){
-			email = occaptmock.getEmail();
-		}
-		}
-		
-		userservicemockimpl.deleteUser(email);
-		List <User> userlist=userservicemockimpl.listUsers();
-		assertEquals(2, userlist.size());
-
-		occaptDaoImplMock.deleteOccupiedApartment("Example unit");
-
-		occlist = occaptDaoImplMock.occupiedApartmentsList();
-
-		assertEquals(2, occlist.size());
+		List<Occupied_apartment> list = occuAptDAO.occupiedApartmentsList();
+		 assertEquals("pass","999",list.get(list.size()-1).getUnit());
+		 assertEquals("pass","a11",list.get(list.size()-1).getEmail());
 	}
 }
